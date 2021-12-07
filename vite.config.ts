cb15822e4831dac9796f4e85834d19f657122b16
ellawu1 <ella.wu1@ingka.ikea.com>
2021-12-07 14:46:42 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import styleImport from 'vite-plugin-style-import';
+import styleImport from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),styleImport({
-    libs: [
-      {
-        libraryName: '@nutui/nutui',
-        libraryNameChangeCase: 'pascalCase',
-        resolveStyle: (name) => {
-          return `@nutui/nutui/dist/packages/${name}/index.scss`
+  plugins: [
+    vue(),
+    styleImport({
+      libs: [
+        {
+          libraryName: '@nutui/nutui',
+          libraryNameChangeCase: 'pascalCase',
+          resolveStyle: (name) => {
+            return `@nutui/nutui/dist/packages/${name}/index.scss`
+          }
         }
-      }
-    ],
-  }),],
+      ]
+    })
+  ],
   css: {
     preprocessorOptions: {
       scss: {
         // 配置 nutui 全局 scss 变量
-        additionalData: `@import "@nutui/nutui/dist/styles/variables.scss";`
+        additionalData: '@import "@nutui/nutui/dist/styles/variables.scss";'
       }
     }
   }
